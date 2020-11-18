61c3bc120c315baa02fbc8dc2a533cae3ef8986f
Mihai Eugen Miuta <miuta.mihai@gmail.com>
2020-11-18 13:23:02 +0200

    Correct script (#3)

--- docker_install/docker_install.sh
+++ docker_install/docker_install.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
-# Shell script to add docker-ce to Deepin Linux repositories
+sudo apt-get remove docker docker-engine docker.io containerd runc
 
-# Remove old docker
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo apt-get update
 
-# Install dependencies
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
 
-# Add GPG and fingerprint
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# Add debian repository in deepin sources list
-printf 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable\n' \
-| sudo tee /etc/apt/sources.list.d/docker-ce.list
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
-# Update packages and install
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
 
-# Add current user to the docker users group (OPTIONAL)
-sudo usermod -aG docker $(whoami)n
+sudo apt-get update
 
-# Download current version of Docker-Compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# Apply executable permissions
 sudo chmod +x /usr/local/bin/docker-compose
 
-echo "Remember to log out before using the new commands"
+echo "Done! Please restart your computer"
